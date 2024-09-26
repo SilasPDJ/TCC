c8eec933f77e063898383568ebf65a99d9abeacb
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2024-09-26 14:34:24 -0300

    separa o footer + fix imagem

---
+++ frontend/html/utils/footer.php
@@ -0,0 +1,26 @@
+    <!-- PARTE DE BAIXO -->
+    <footer class="footer baixo py-3">
+        <div class="container">
+            <div class="row itens">
+                <div class="col-md-4">
+                    <h4>ASL Libras</h4>
+                    <ul class="list-unstyled">
+                        <li><a class="footerA" href="#">Sobre nós</a></li>
+                        <li><a class="footerA" href="#">Contato</a></li>
+                        <li><a class="footerA" href="#">Termos de uso</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Contatos</h4>
+                    <p>Telefone: (11) 1234-5678</p>
+                    <p>E-mail: contato@asl.com</p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Siga-nos</h4>
+                    <button class="btnFooter"><i class="fa-brands btns fa-instagram"></i></button>
+                    <button class="btnFooter"><i class="fa-brands btns fa-square-facebook"></i></button>
+                    <button class="btnFooter"><i class="fa-brands btns fa-youtube"></i></button>
+                </div>
+            </div>
+        </div>
+    </footer>
