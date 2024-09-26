f29d565853dd6819e5db97e629b00145b1c97b12
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2024-09-26 11:00:43 -0300

    rename to frontend folder

--- LibrasASL-master-main/Js/videos.js
+++
@@ -1,4 +0,0 @@
-  function toggleDescricao(descricaoId) {
-    var descricaoElement = document.getElementById('descricao-' + descricaoId);
-    descricaoElement.style.display = descricaoElement.style.display === 'none' ? 'block' : 'none';
-  }
